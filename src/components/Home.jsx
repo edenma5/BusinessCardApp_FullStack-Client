c4d4b8d8e377ce2b7310d7ccf3d9735e12d3248c
edenma5 <edenmaimon8@gmail.com>
2022-10-31 21:19:20 +0200

    try background mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -31,7 +31,7 @@ export default function Home() {
             {userInfo ?
                 <div className='homePageContentContainer'>
                     <div className='homePageContent'>
-                        <span style={{ fontWeight: 'bolder', fontSize: '1.2em', color: '#46364a', letterSpacing: '2px' }}>{textByTime()} </span><span className='specialWords' style={{ color: '#528265' }}> {userInfo.fullName}</span><span className="emojiHand">👋🏼</span><br /><br />
+                        <span style={{ fontWeight: 'bolder', fontSize: '1.2em', color: '#46364a', letterSpacing: '2px' }}>{textByTime()} </span><span className='specialWords' style={{ color: 'rgb(132, 160, 150)' }}> {userInfo.fullName}</span><span className="emojiHand">👋🏼</span><br /><br />
                         In <span style={{ cursor: 'pointer', color: 'rgb(0, 101, 101)', fontWeight: 'bolder', fontSize: '1.2em' }} onClick={() => navigate('/allbusinesscards')}>All Cards</span> tab you can find all the business cards which belongs to our business customers, who will be happy to provide you with professional, quality and kindly service.<br />
                         From small to large size businesses, everything here is in a simple and convenient system.<br /><br />
                         {userInfo?.isBusinessAccount && <p>In your <span style={{ cursor: 'pointer', color: 'rgb(0, 101, 101)', fontWeight: 'bolder', fontSize: '1.2em' }} onClick={() => navigate('/managmentcards')}>Personal Area</span>, You can always edit or delete your cards, And of course to create new business cards, as many as you want!</p>}
@@ -39,7 +39,7 @@ export default function Home() {
                 </div> :
                 <div className='homePageContentContainer'>
                     <div className='homePageContent'>
-                        <span className='specialWords' style={{ color: '#528265' }}>Welcome</span> to the best business card application in the world!<br /><br />
+                        <span className='specialWords' style={{ color: 'rgb(132, 160, 150)' }}>Welcome</span> to the best business card application in the world!<br /><br />
                         It seems that you are not logged in yet.<br />
                         We invite you to register by clicking <span style={{ cursor: 'pointer', color: 'rgb(0, 101, 101)', fontWeight: 'bolder', fontSize: '1.2em' }} onClick={() => navigate('/signup')}>here!</span> <span className='specialWords' style={{ color: 'lightsteelblue', rotate: '10deg', fontSize: '1em', marginLeft: '3rem' }}>It's free!!</span><br />
                         Or <span style={{ cursor: 'pointer', color: 'rgb(0, 101, 101)', fontWeight: 'bolder', fontSize: '1.2em' }} onClick={() => navigate('/signin')}>sign in</span> if you already have an account.<br />
